b0f32a4bc2218bb80ef1b36215875a756fc4eb4a
ThememoriesIII <ThememoriesIII@users.noreply.github.com>
2023-07-20 10:46:57 +0700

    Update face_recognizer.py

--- face_recognizer.py
+++ face_recognizer.py
@@ -2,23 +2,24 @@ import cv2 #อิมพอทโมดูลไลบรารี่ของ o
 import numpy as np #อิมพอทโมดูลไลบรารี่ numpy อ่านว่านัมพาย หรือ นัมปี้ เป็นโมดูลสำหรับคำนวณทางคณิตศาสตร์สูตรบางที่อยู่นอกเหรือจากไลบรารี่พื้นฐานของไพทอนจะใช้ไลบรารี่นี้แทนเนื่อจากบ่งสูตรนั้นมีความซัพซ้อนในการเขียนหากเขียนเองอาจมีความผิดพลาดได้หรืออาจช้ากว่าเดิม
 import os #อิมพอทโมดูลไลบรารี่ OS เข้ามาในโปรแกรมเป็นโมดูลสำหรับเรียกใช้งานฟังชั่นของวินโดว
 
-recognizer = cv2.face.LBPHFaceRecognizer_create() #
-recognizer.read('trainer.yml')
+recognizer = cv2.face.LBPHFaceRecognizer_create() #สร้างไฟล์ตัวแปลชื่อเรคคอไนทเซอร์สำหรับเป็นที่เก็บออปเจ็ค lowbinarypatternhistrogram เป็น object สำหรับใช้ในการตรวจสอบคุณสมบัต่ของรูปโปรไฟล์
+                                                #binary profile แตกต่างกับ low binary profile โดยไบนารี่โปรไฟล์จะแทนสีด้วยสีสองสีคือขาว ดำแทนด้วยตัวเลข 0 และ 1 ส่วนโลวโปรไฟล์จะแทนด้วยเฉดสีดำไปหาขาวแทนตัวเลขด้วย 0-255
+recognizer.read('trainer.yml') #โหลดโมเดลหน้าสำหรับให้ AI ตรวจสอบจากไฟล์ชื่อ trainer.yml
 
-face_cascade_Path = "haarcascade_frontalface_default.xml"
+face_cascade_Path = "haarcascade_frontalface_default.xml" #โหลดเซ็นข้อมูลที่ใช้สำหรับกำหนดพื้นที่ตรวจสอบใบหน้าจากไฟล์  haarcascade_frontalface_default.xml เก็บในตัวแปลชื่อ face_cascade_Path
 
 
-faceCascade = cv2.CascadeClassifier(face_cascade_Path)
+faceCascade = cv2.CascadeClassifier(face_cascade_Path)#กำหนดไฟล์พื้นที่สำหรับตรวจสอบใบหน้าของแต่ละหน้าที่บันทึกจากไฟล์  haarcascade_frontalface_default.xml
 
-font = cv2.FONT_HERSHEY_SIMPLEX
+font = cv2.FONT_HERSHEY_SIMPLEX #กำหนดฟร้อนที่ใช้สำหรับตรวจจับใบหน้า
 
-id = 0
+id = 0 #ตัวแปล id ใช้สำหรับเก็บ index รูปภาพที่ใช้สำหรับตรวจสอบ
 # names related to ids: The names associated to the ids: 1 for Mohamed, 2 for Jack, etc...
-names = ['None'] # add a name into this list
+names = ['None'] # add a name into this list #ตัวแปล names ใช้สำหรับเก็บ ชื่อของรูปภาพหรือสิ่งของที่ต้องการตรวจจับโดยชื่อแรกจะเป็น  NONE ชื่อสำหรับหน้าที่ไม่ได้ทำการจดจำหรือทำการเทรนให้ AI รู้จัก
 #Video Capture
-cam = cv2.VideoCapture(0)
-cam.set(3, 640)
-cam.set(4, 480)
+cam = cv2.VideoCapture(0) #คำสั่งสำหรับให้เลือก video cature ที่ 0 หรือก็คือ กล้องเริ่มต้นของอุปกรณ์คอมพิวเตอร์
+cam.set(3, 640)#เซ็ตขนาดึวามกว้างของหน้าจอหน้าจอ
+cam.set(4, 480)#เซ็ตขนาดความสูงของหน้าจอหน้าจอ
 # Min Height and Width for the  window size to be recognized as a face
 minW = 0.1 * cam.get(3)
 minH = 0.1 * cam.get(4)
